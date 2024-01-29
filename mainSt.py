2a858e2e3901734998717d4f87b96012db9ce955
pritidodhia <148013440+pritidodhia@users.noreply.github.com>
2024-01-29 23:27:35 +0000

    Add files via upload

---
+++ mainSt.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+# st.title("Welcome to wscube tech")
+# st.header("python")
+# st.subheader("java")
+# st.markdown("I love Python")
+# st.code("""for i in range(1,5,1):
+#               print("hello")
+#         """)
+name = st.text_input("Enter your name : ")
+fname = st.text_input("Enter your father name : ")
+adr = st.text_area("Enter your Text : ")
+classdata = st.selectbox("Enter Your Class : ", (1,2,3,4,5,6))
+
+button = st.button("Done")
+if button :
+    st.markdown(f"""
+    Name : {name}
+    Father Name : {fname}
+    address : {adr}
+    class : {classdata}""")
